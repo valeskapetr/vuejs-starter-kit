64c9692d4e7fca76f4109b2d19373f8e98d2686f
Petr Valeška <kucharik59@gmail.com>
2025-07-15 17:40:04 +0200

    // Main updates

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="relative py-[150px] flex justify-center items-center">
+        <div class="absolute -top-28 -left-28 bg-[#4bd64b] size-[20rem] lg:size-[30rem] blur-[250px]"/>
+        <div class="absolute -bottom-[28rem] -right-28 bg-[#4bd64b] size-[20rem] lg:size-[30rem] blur-[250px]"/>
+        
+        <div class="relative z-10 m-auto px-4 w-[95%]">
+            <div class="flex flex-col-reverse 2xl:flex-row 2xl:justify-between">
+                <div class="col my-auto">
+                    <h1 class="text-4xl md:text-6xl text-center 2xl:text-start text-transparent font-bold bg-gradient-to-r from-[#4bd64b] to-white bg-clip-text mb-4">Welcome to <strong>Vue.js Starter KIT</strong>!</h1>
+                    <p class="text-center 2xl:text-start text-xl text-zinc-500 max-w-4xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In voluptatem assumenda sequi quasi. Libero odio cupiditate numquam, provident molestias dignissimos voluptas laborum, dolores optio magni fuga voluptates ad aspernatur inventore.</p>
+                </div>
+                <div class="col my-auto">
+                    <img src="/vuejs.webp" alt="" class="relative z-5 w-[380px] block mx-auto 2xl:ml-auto [filter:drop-shadow(0_0_80px_#4bd64b60)]"/>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
